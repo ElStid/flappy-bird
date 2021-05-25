69ab1b3388cb09c11f2be582c83e73f1df68f154
ElStid <72676854+ElStid@users.noreply.github.com>
2021-05-25 09:52:52 -0500

    #4

--- Assets/Scrip/L_puntage.cs
+++ Assets/Scrip/L_puntage.cs
@@ -1,14 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
-using TMPro;
 using UnityEngine;
+using TMPro;
+
 
 public class L_puntage : MonoBehaviour
 {
-    public static int puntaje;
+    public static int puntaje = 0;
     void Start()
     {
         puntaje = 0;
+        
     }
 
     void Update()
